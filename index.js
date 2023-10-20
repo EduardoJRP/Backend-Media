a57760a5e805f037d1dc2f25a7442ac2122106ad
Eduardo Rojas <edu.rojas.paez@gmail.com>
2023-10-20 11:30:38 -0500

    feat: Added connection to the database

--- index.js
+++ index.js
@@ -5,8 +5,30 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+const database = module.exports = () => {
+    const connectionParams = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+
+    try {
+        mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true});
+        console.log('Connected to the database')
+    } catch (error) {
+        console.log('Error on database')
+    }
+}
+
 dotenv.config();
 
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan());
+
+
 app.listen(8800, () => {
-    console.log("Hello World!")
+    console.log("Connected to the server")
 })
+
+database();
