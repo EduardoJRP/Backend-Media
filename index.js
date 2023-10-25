79c028863c9b9b132eb2d38eeef332d5f5427f0d
Eduardo Rojas <edu.rojas.paez@gmail.com>
2023-10-25 18:45:23 -0500

    feat: Added posts file for CRUD functionality

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const morgan = require("morgan");
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 const database = module.exports = () => {
     const connectionParams = {
@@ -30,7 +31,8 @@ app.use(helmet());
 app.use(morgan());
 
 app.use("/api/users", userRoute);
-app.use("/api/auth", authRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
     console.log("Connected to the server")
--- routes/users.js
+++ routes/users.js
@@ -52,6 +52,46 @@ router.get("/:id", async (req,res) => {
 });
 
 // Follow User
+router.put("/:id/follow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (!user.followers.includes(req.body.userId)) {
+                await user.updateOne({$push:{followers: req.body.userId}});
+                await currentUser.updateOne({$push:{following: req.params.id}});
+                res.status(200).json("You follow this user now")
+            } else {
+                res.status(403).json("You already follow this user")
+            }
+        } catch(err) {
+            res.status(500).json.err
+        }
+    } else {
+        res.status(403).json("You cant follow yourself");
+    }
+});
+
 // Unfollow User
+router.put("/:id/unfollow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (user.followers.includes(req.body.userId)) {
+                await user.updateOne({$pull:{followers: req.body.userId}});
+                await currentUser.updateOne({$pull:{following: req.params.id}});
+                res.status(200).json("You unfollow this user now")
+            } else {
+                res.status(403).json("You already unfollow this user")
+            }
+        } catch(err) {
+            res.status(500).json.err
+        }
+    } else {
+        res.status(403).json("You cant unfollow yourself");
+    }
+});
+
 
 module.exports = router
