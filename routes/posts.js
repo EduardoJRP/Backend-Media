cbc4ba7cbb1b171c81eb7bdc0643c421991eac3d
Eduardo Rojas <edu.rojas.paez@gmail.com>
2023-10-26 18:51:36 -0500

    feat: Functionalities for the posts

--- routes/posts.js
+++ routes/posts.js
@@ -58,4 +58,32 @@ router.put("/:id/like", async (req, res) => {
     }
 });
 
+// Get a post
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// FIX !!!!
+// Get timeline posts
+router.get("/timeline/all", async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({ userId: currentUser._id });
+        const friendPosts = await Promise.all(
+            currentUser.following.map((friendId) => {
+                return Post.find({ userId: friendId });
+            })
+        );
+        res.status(200).json(userPosts.concat(...friendPosts));
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
